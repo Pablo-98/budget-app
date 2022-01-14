a90e154307b7350c86d15b5158c99aba4b5ff949
Pablo Hernandez <hpablo575@gmail.com>
2022-01-13 17:52:46 -0700

    added files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { Button, Stack } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container'
+import BudgetCard from './components/BudgetCard';
 
  function App() {
-  return  <Container>
+  return ( 
+  <Container className="my-4" >
     <Stack direction="horizontal" gap="2" className="mb-4">
       <h1 className="me-auto">Budgets</h1>
       <Button variaint="primary">Add Budget</Button>
       <Button variaint="outline-primary">Add Expense</Button>
-
-    </Stack>
-
-  </Container>
+      </Stack>
+      <div 
+      style= {{
+         display:"grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+         alignItems: "flex-start",
+        }}
+        >
+          <BudgetCard> </BudgetCard>
+        </div>
+      </Container>
+  )
 }
 
 export default App;
