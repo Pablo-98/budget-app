66c34da7ba6e6ad17560090f2a8a8a2e52b254e6
Pablo Hernandez <hpablo575@gmail.com>
2022-01-18 18:47:05 -0700

    added button to BudjetModal and modified file

--- src/components/contexts/BudgetsContext.js
+++ src/components/contexts/BudgetsContext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import {v4 as uuidV4 } from 'uuid'
+import useLocalStorage from '../../hooks/useLocalStorage'
 
 const BudgetsContext = React.createContext()
 
@@ -8,16 +9,16 @@ export function useBudgets() {
 }
 
 export const BudgetsProvider = ({ children }) => {
-    const [budgets, setBudgets] = useState([])
-    const [expenses, setExpenses] = useState([])
+    const [budgets, setBudgets] = useLocalStorage("budgets", [])
+    const [expenses, setExpenses] = useLocalStorage("expenses", [])
 
     function getBudgetExpenses (budgetId) {
         return expenses.filter(expense => expense.budgetId === budgetId)
 
     }
-        function addExpense(description, amount, budgetId) {
+        function addExpense({description, amount, budgetId}) {
             setExpenses(prevExpenses => {
-                    return [...prevExpenses, { id: uuidV4(), name, max }]
+                    return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }]
                 
             })
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BudgetsProvider } from './components/contexts/BudgetsContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BudgetsProvider>
+      <App /> 
+
+    </BudgetsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
